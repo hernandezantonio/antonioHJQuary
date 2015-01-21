a42ab2c53741dc0fb6d51d19e50ef44eac5cbbf0
antonio hernandez <defygaming99@gmail.com>
2015-01-21 11:27:38 -0800

    new stuff

--- public_html/script.js
+++ public_html/script.js
@@ -3,7 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+//this is all the stuff i got from the videos these deals with every cool thing 
+//on the home page. 
 $("document").ready(function(){
     $(".defy").css("background-color", "green"); 
     $("p").css("color", "black");
@@ -15,11 +16,13 @@ $("document").ready(function(){
     $('#hide').bind('click', hideThePage);
     $('#show').bind('click', showThePage);
     $('#superHumans').accordion({header: "h3"});
-    $('#superHumans').css('width', '400px')
+    $('#superHumans').css('width', '400px');
     $('h3').css('background', 'red');
 });
 
-
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+//functions 
+//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
 function hideThePage(){
     $('#show').css('visibility', 'visible');
@@ -49,14 +52,16 @@ function mouseOverMe (){
 }
 
 function mouseOverMe (){
-    $("h1").html("i <3 nadeshot");
+    $("p").html("i <3 nadeshot");
 }
-
+//this code makes it so that when i click on the header on my page every text turns 
+//into what i wrote down down there 
+    
 function mouseClick (){
     
-    $("p").html("YOU ARE IN THE MIDDLE OF A DDOS ")
+    $("p").html("YOU ARE IN THE MIDDLE OF A DDOS ");
 }
 
 function addAPara() {
     $('#randPara').append('Aye Defy');
-}
+} 
